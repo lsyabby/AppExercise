266d3c022dcc0edf88100eb3202caae2bcdb02a2
lsyabby <amyandlisa21022@gmail.com>
2021-06-20 01:34:30 +0800

    [Modify] modified APIService

--- AppExercise/List/ListViewController.swift
+++ AppExercise/List/ListViewController.swift
@@ -7,10 +7,34 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDelegate {
+    
+    private let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    private func setupTableView() {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.backgroundColor = .brown
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        tableView.showsHorizontalScrollIndicator = false
+        
+        [
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            
+        ].forEach {
+            $0.isActive = true
+        }
     }
 
     deinit {
---
+++ AppExercise/List/ListViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ListViewModel.swift
+//  AppExercise
+//
+//  Created by 李思瑩 on 2021/6/20.
+//
+
+import UIKit
+
+class ListViewModel {
+    
+    
+    
+}
